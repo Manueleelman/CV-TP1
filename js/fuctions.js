e444bb6c4baaae61cda591f6b85033426f429ed6
Manuel Arroyo <manuelarroyoperalta@gmail.com>
2023-03-01 00:50:07 -0300

    commit del tp

---
+++ js/fuctions.js
@@ -0,0 +1,6 @@
+document.getElementById('boton').addEventListener('click', function() {
+  document.getElementById("este").innerHTML = "Goku necesita tu ayuda"
+
+  document.body.style.backgroundColor = '#F65533'
+  document.getElementById('vege').src= "C:\Users\PC\OneDrive\Escritorio\Manuel\Ds P\goku.jpg";
+})
